0d33cb904c121bf944bd98a115855514cfc898f9
Priyanka Agrawal <pragrawal@microsoft.com>
2021-05-15 23:30:12 +0530

    Projects page redesign

---
+++ script.js
@@ -0,0 +1,32 @@
+//const body = document.querySelector(body);
+
+const projectMap = {
+    "Generate Quotes": "/JS-20/Generate Quotes/index.html",
+    "Infinite Scroll": "/JS-20/Infinite Scroll/index.html",
+    "Picture in Picture": "/JS-20/Picture in Picture/index.html",
+    "Joke Teller": "/JS-20/Joke-Teller/index.html",
+    "Light Dark Mode": "/JS-20/animated-template/index.html",
+    "Animated Template": "/JS-20/animated-template/index.html",
+    "Animated Navigation": "/JS-20/animated-navigation/index.html",
+    "Music Player": "/JS-20/music-player/index.html",
+    "Countdown": "/JS-20/custom-countdown/index.html",
+    "Bookmark keeper": "/JS-20/Bookmark keeper/index.html"
+};
+function cardClick(e) {
+    const target = e.target.alt;
+    let url ='';
+    if(projectMap[target] != ''){
+        let link = projectMap[target];
+        url = `${window.location.origin}${link}`;
+        console.log(url);
+    window.open(url, '_blank').focus();
+    }
+    
+    
+
+}
+
+window.addEventListener('click', cardClick);
+
+
+
